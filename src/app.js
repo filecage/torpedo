935f6193dce8b5befc73435d7719f824d1ed303c
David Beuchert <mail@dbeuchert.com>
2016-03-16 12:58:27 +0100

    default app plays with test platform and random strategy

--- src/app.js
+++ src/app.js
@@ -1,16 +1,7 @@
 var Game = require('./Game');
-var Grid = require('./Game/Grid');
-var Field = require('./Game/Field');
+var TestPlatform = require('./Platforms/Test');
+var RandomStrategy = require('./Strategies/Random');
 
-var mine = new Grid(10, 10);
-var enemy = new Grid(10, 10);
+var game = new Game(new TestPlatform(), new RandomStrategy());
 
-var game = new Game(mine, enemy);
-
-enemy.setFieldState(3, 3, Field.STATE.MISSED);
-enemy.setFieldState(1, 1, Field.STATE.HIT);
-enemy.setFieldState(1, 2, Field.STATE.HIT);
-enemy.setFieldState(6, 5, Field.STATE.MISSED);
-enemy.setFieldState(5, 6, Field.STATE.MISSED);
-
-console.log(game._findFinishingFields(enemy));
+game.play();
