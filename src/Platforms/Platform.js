fd74154e387a897a658d01421c92ef8f890b8237
David Beuchert <mail@dbeuchert.com>
2016-03-16 12:47:21 +0100

    added basic abstract platform

---
+++ src/Platforms/Platform.js
@@ -0,0 +1,20 @@
+'use strict';
+
+/**
+ * @abstract
+ */
+class Platform {
+    /**
+     * @return {Grid}
+     */
+    getEnemyGrid() {
+        throw new Error('Platform must implement method getEnemyGrid()');
+    }
+
+    /**
+     * @param {Field} field
+     */
+    shootAt(field) {
+        throw new Error('Platform must implement method shootAt')
+    }
+}
