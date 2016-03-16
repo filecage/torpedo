1f1cb04fbc68566c3f90b2684dc17ab71acecc54
David Beuchert <mail@dbeuchert.com>
2016-03-16 18:14:23 +0100

    be able to create a random fleet with pre-defined ship sizes

--- src/Game/Fleet.js
+++ src/Game/Fleet.js
@@ -1,8 +1,66 @@
 'use strict';
 
+var _ = require('lodash');
 var Ship = require('./Ship');
+var Field = require('./Field');
 
 class Fleet {
+
+    /**
+     * @param {Grid} grid
+     * @param shipSizes
+     */
+    static createRandomFleetOnGrid(grid, shipSizes) {
+        var fleet = new Fleet();
+
+        shipSizes.forEach(shipSize => {
+            var possibleAreas = [];
+
+            // checking if the complete area is free
+            grid.getFieldsByState(Field.STATE.UNKNOWN)
+                .forEach(field => {
+                    var horizontal = grid.getAreaFields(field.getX(), field.getY(),shipSize, 1);
+                    var vertical = grid.getAreaFields(field.getX(), field.getY(), 1, shipSize);
+
+                    if (Field.filterByState(horizontal, Field.STATE.UNKNOWN).length === shipSize) {
+                        possibleAreas.push({
+                            x: field.getX(),
+                            y: field.getY(),
+                            width: shipSize,
+                            height: 1
+                        });
+                    }
+
+                    if (Field.filterByState(vertical, Field.STATE.UNKNOWN).length === shipSize) {
+                        possibleAreas.push({
+                            x: field.getX(),
+                            y: field.getY(),
+                            width: 1,
+                            height: shipSize
+                        });
+                    }
+                });
+
+            // verify that there is no neighbor ship
+            possibleAreas = possibleAreas.filter(area => {
+                return !grid.getAreaNeighborsWithDiagonal(area.x, area.y, area.width, area.height).some(neighbor => {
+                    return !!fleet.getShipAtPosition(neighbor.getX(), neighbor.getY());
+                });
+            });
+
+            if (!possibleAreas.length) {
+                throw new Error('unable to place all ships on the battlefield');
+            }
+
+            // choose some random area and place the ship
+            var position = _.shuffle(possibleAreas).pop();
+            fleet.addShip(new Ship(grid.getAreaFields(position.x, position.y, position.width, position.height)));
+        });
+
+        return fleet;
+
+    }
+
     constructor () {
         this._ships = [];
     }
