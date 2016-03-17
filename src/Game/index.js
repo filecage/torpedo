fbd455088b138d9e928ea628eaf3412957ae8292
David Beuchert <mail@dbeuchert.com>
2016-03-17 17:30:45 +0100

    isolates hunting of fragged ships to own strategy

--- src/Game/index.js
+++ src/Game/index.js
@@ -26,18 +26,7 @@ class Game {
 
     _turn () {
         debug('my turn');
-        var target = null;
-        var grid = this._platform.getEnemyGrid();
-
-        debug('finding fields to frag an already hit ship..');
-        var finishingFields = this._findFinishingFields(grid);
-        if (finishingFields.length) {
-            debug('found %d finishing fields', finishingFields.length);
-            target = finishingFields.shift();
-        } else {
-            debug('no finishing fields found, getting target by strategy');
-            target = this._strategy.getTargetField(grid);
-        }
+        var target = this._strategy.getTargetField(this._platform.getEnemyGrid());
 
         if (target) {
             debug('shooting at %d/%d', target.getX(), target.getY());
@@ -45,31 +34,6 @@ class Game {
         } else {
             debug('no target found, is the game over?');
         }
-    }
-
-    _findFinishingFields (grid) {
-        var finishingFields = [];
-        var hit = grid.getFieldsByState(Field.STATE.HIT);
-        hit.forEach(field => {
-            finishingFields = finishingFields.concat(
-                grid.getFieldNeighbors(field)
-                    .filter(neighbor => {
-                        return this._isShootable(grid, neighbor);
-                    })
-            );
-        });
-
-        finishingFields.sort((a, b) => {
-            return grid.getFieldNeighborsByState(b, Field.STATE.HIT, 2).length - grid.getFieldNeighborsByState(a, Field.STATE.HIT, 2).length;
-        });
-
-        return finishingFields;
-    }
-
-    _isShootable(grid, field) {
-        return field.getState() === Field.STATE.UNKNOWN && grid.getFieldNeighbors(field).filter(neighbor => {
-            return neighbor.getState() >= Field.STATE.SUNK;
-        }).length === 0;
     }
 }
 
---
+++ src/Strategies/Hunter.js
@@ -0,0 +1,60 @@
+'use strict';
+
+var StrategyInterface = require('./StrategyInterface');
+var Field = require('../Game/Field');
+var RandomStrategy = require('./Random');
+
+class Hunter extends StrategyInterface {
+
+    constructor() {
+        super();
+        this._randomStrategy = new RandomStrategy;
+    }
+
+    getTargetField (grid) {
+        debug('finding fields to frag an already hit ship..');
+        var finishingFields = this.findFinishingFieldsOnGrid(grid);
+        if (finishingFields.length) {
+            debug('found %d finishing fields', finishingFields.length);
+            return finishingFields.shift();
+        } else {
+            debug('no finishing fields found, getting target by strategy');
+            return this._randomStrategy.getTargetField(grid);
+        }
+    }
+
+    /**
+     * @param {Grid} grid
+     * @returns {Array}
+     */
+    findFinishingFieldsOnGrid (grid) {
+        var finishingFields = [];
+        var hit = grid.getFieldsByState(Field.STATE.HIT);
+        hit.forEach(field => {
+            finishingFields = finishingFields.concat(
+                grid.getFieldNeighbors(field)
+                    .filter(neighbor => {
+                        return this._isShootable(grid, neighbor);
+                    })
+            );
+        });
+
+        finishingFields.sort((a, b) => {
+            return grid.getFieldNeighborsByState(b, Field.STATE.HIT, 2).length - grid.getFieldNeighborsByState(a, Field.STATE.HIT, 2).length;
+        });
+
+        return finishingFields;
+    }
+
+    /**
+     * @param {Grid} grid
+     * @param {Field} field
+     * @returns {boolean}
+     * @private
+     */
+    _isShootable(grid, field) {
+        return field.getState() === Field.STATE.UNKNOWN && grid.getFieldNeighbors(field).filter(neighbor => {
+            return neighbor.getState() >= Field.STATE.SUNK;
+        }).length === 0;
+    }
+}
